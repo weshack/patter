34c07bbe7726b52c02109064b1df2c4dc67c95ff
Joomy Korkut <joomy@cattheory.com>
2016-02-20 16:12:50 -0500

    Switch to p5

--- js/patter.js
+++ js/patter.js
@@ -1,1 +1,26 @@
-function getProcessingSketchId () { return 'procapp'; }
+var patch;
+
+var setup = function () {
+  // start Pure Data
+  $.get('patter-synth.pd', function(patchStr) {
+    patch = Pd.loadPatch(patchStr);
+    Pd.start();
+  });
+
+  // start graphics
+  createCanvas(640, 480);
+  stroke(0);
+  fill(150);
+};
+
+// Drawer function that continuously loops
+var draw = function () {
+  background(255);
+  rect(50,50,75,100);
+  ellipse(mouseX, mouseY, 80, 80);
+};
+
+var checkGrid = function (grid) {
+  console.log(grid);
+  return;
+};
