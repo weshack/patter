353d1c1cfb243563e5add749e837c5e0ec4dcd41
Jason Wangsadinata <jwangsadinata@users.noreply.github.com>
2016-02-20 17:34:50 -0500

    update with pattern checking

--- js/patter.js
+++ js/patter.js
@@ -47,3 +47,122 @@ var checkGrid = function (grid) {
   console.log(grid);
   return;
 };
+
+function isShape0(x,y) {
+  var cellsToCheck = [
+  [x-1, y-1],
+  [x-1, y],
+  [x-1, y+1]
+  [x, y-1],
+  [x, y+1],
+  [x+1, y-1],
+  [x+1, y],
+  [x+1, y+1]];
+  var tempx,tempy;
+  for (var i = 0; i < cellsToCheck.length; i++) {
+    tempx = cellsToCheck[i][0];
+    tempy = cellsToCheck[i][1];
+    if(!(grid[x][y] == "black" && grid[tempx][tempy] == "black")) return false;
+  }
+  return true;
+}
+
+function isShape1(x,y) {
+  var cellsToCheck = [
+  [x, y+1],
+  [x, y+2],
+  [x+1, y],
+  [x+1, y+2],
+  [x+2, y],
+  [x+2, y+1],
+  [x+2, y+2]];
+  var tempx,tempy;
+  for (var i = 0; i < cellsToCheck.length; i++) {
+    tempx = cellsToCheck[i][0];
+    tempy = cellsToCheck[i][1];
+    if(!(grid[x][y] == "black" && grid[tempx][tempy] == "black")) return false;
+  }
+  return true;
+}
+
+function isShape2(x,y) {
+  var cellsToCheck = [
+  [x, y+2],
+  [x+2, y],
+  [x+2, y+2]];
+  var tempx,tempy;
+  for (var i = 0; i < cellsToCheck.length; i++) {
+    tempx = cellsToCheck[i][0];
+    tempy = cellsToCheck[i][1];
+    if(!(grid[x][y] == "black" && grid[tempx][tempy] == "black")) return false;
+  }
+  return true;
+}
+
+function isShape3(x,y) {
+  var cellsToCheck = [
+  [x, y-1],
+  [x, y+1],
+  [x+2, y]];
+  var tempx,tempy;
+  for (var i = 0; i < cellsToCheck.length; i++) {
+    tempx = cellsToCheck[i][0];
+    tempy = cellsToCheck[i][1];
+    if(!(grid[x][y] == "black" && grid[tempx][tempy] == "black")) return false;
+  }
+  return true;
+}
+
+function isShape4(x,y) {
+  var cellsToCheck = [
+  [x-1, y-1],
+  [x-1, y+1],
+  [x+1, y-1],
+  [x+1, y+1]];
+  var tempx,tempy;
+  for (var i = 0; i < cellsToCheck.length; i++) {
+    tempx = cellsToCheck[i][0];
+    tempy = cellsToCheck[i][1];
+    if(!(grid[x][y] == "black" && grid[tempx][tempy] == "black")) return false;
+  }
+  return true;
+}
+
+function isShape5(x, y) {
+  var cellsToCheck = [
+  [x, y+1],
+  [x, y-1],
+  [x+1, y],
+  [x-1, y]];
+  var tempx,tempy;
+  for (var i = 0; i < cellsToCheck.length; i++) {
+    tempx = cellsToCheck[i][0];
+    tempy = cellsToCheck[i][1];
+    if(!(grid[x][y] == "black" && grid[tempx][tempy] == "black")) return false;
+  }
+  return true;
+}
+
+var drawColor = function (x, y) {
+  if (isShape0(x,y)) {
+    // 
+  }
+  else if (isShape1(x,y)) {
+    //
+  }
+  else if (isShape2(x,y)) {
+    //
+  }
+  else if (isShape3(x,y)) {
+    //
+  }
+  else if (isShape4(x,y)) {
+    //
+  }
+  else if (isShape5(x,y)) {
+    //
+  }
+  else {
+    //
+  }
+}
